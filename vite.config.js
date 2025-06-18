c75ec0a5ef2f5ee5e7fe567c5979506c68e9f895
Faiz56-16 <widanafaiza@gmail.com>
2025-06-18 08:59:13 +0700

    First commit

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
     plugins: [
@@ -8,4 +9,7 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+            server: {
+            host: true
+        }
 });
